e2466edfaa5b85229829cc2b223b88463c991299
Nikhil <nikhil.celtics@gmail.com>
2017-07-21 16:21:21 +0530

    username is now asked

--- chat.c
+++ chat.c
@@ -22,13 +22,12 @@ void *get_in_addr(struct sockaddr *sa){
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(){
+int main(int c, char* argv[]){
 	printf("Server is running\n");
 	struct sockaddr_storage their_addr;
 	socklen_t addr_size;
 	struct addrinfo hints, *res;
 	int listener, new_fd;
-	char buf[512], s[256];
 	fd_set master, read_fds;
 	int fdmax, nbytes;
 
@@ -48,9 +47,13 @@ int main(){
 	FD_SET(listener, &master);
 	fdmax = listener;
 
-	char msg[] = "Welcome to the chat!\n";
-	char okay[] = "OK";
-	char bye[] = "If you say so.";
+	char welcome[] = "Welcome to the chat!\nEnter username: ";
+	char client_ip[40];
+	char msg[50];
+	int nicked_users[10];
+	char nicks_list[10][50];
+	int user_count = 0;
+	int nick_given;
 
 	while (1){
 		read_fds = master;
@@ -67,19 +70,19 @@ int main(){
 						perror("accept");
 					}
 					else{
-						inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
-						printf("Established connection to %s\n", s);
+						inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), client_ip, sizeof client_ip);
+						printf("Established connection to %s\n", client_ip);
 						FD_SET(new_fd, &master);
 						if (new_fd>fdmax){
 							fdmax = new_fd;
 						}
-						if (send(new_fd, msg, strlen(msg)+1, 0) == -1){
+						if (send(new_fd, welcome, strlen(welcome)+1, 0) == -1){
 							perror("send");
 						}
 					}
 				} 
 				else{
-					nbytes = recv(i, buf, sizeof buf, 0);
+					nbytes = recv(i, msg, sizeof msg, 0);
 					if (nbytes < 0){
 						perror("recv");
 					}
@@ -89,17 +92,31 @@ int main(){
 						FD_CLR(i, &master);	
 					}
 					if (nbytes > 0){
-						for (int j=0; j<=fdmax; j++){
-							if (FD_ISSET(j, &master)){
-								if (j!=i && j!=listener){
-									if (send(j, buf, strlen(buf)+1, 0) == -1){
-										perror("send");
+						nick_given = 0;
+						for (int j=0;j<user_count;j++){
+							if (nicked_users[j] == i){
+								nick_given = 1;
+								break;
+							}
+						}
+						if (nick_given == 0){ 
+							nicked_users[user_count] = i;
+							strcpy(nicks_list[user_count], msg);
+							user_count++;
+						}
+						else{
+							for (int j=0; j<=fdmax; j++){
+								if (FD_ISSET(j, &master)){
+									if (j!=i && j!=listener){
+										if (send(j, msg, strlen(msg)+1, 0) == -1){
+											perror("send");
+										}
 									}
 								}
 							}
 						}
 					}
-					memset(buf, '\0', strlen(buf));
+					memset(msg, '\0', strlen(msg));
 				}
 			}
 		}
